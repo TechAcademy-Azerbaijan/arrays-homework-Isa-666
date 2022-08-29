eebb9137279c8c0f355325f6afa386f94ef529ef
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-29 05:09:58 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,17 +4,15 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    
-    let n = result.input.split(" ");
+      let n = result.input.split(" ");
     let arr = [];
 
     for (let i = 0; i < n.length; i++) {
         arr.push(n[i]);
 
 
-    } let a=arr.filter(function(i){return i!==arr[1] && i!==arr[3] && i!==arr[5] })
+    } let a = arr.filter(function (i) { return i !== arr[1] && i !== arr[3] && i !== arr[5] })
 
     console.log(a.join(" "));
-
   
 });
