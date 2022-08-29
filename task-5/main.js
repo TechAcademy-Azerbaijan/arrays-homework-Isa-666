01e81e9246f4722d3eec9944ac4d55cff4527a1b
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-29 16:57:14 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,16 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+       let n = result.input.split(" ") //3 5 -7 7 5 -9 -4
+    let arr = [];
+    for (let i = 0; i < n.length; i++) {
+        arr.push(n[i]);
+
+    }
+    [arr[0], arr[1]] = [arr[1], arr[0]];
+    [arr[2], arr[3]] = [arr[3], arr[2]];
+    [arr[4], arr[5]] = [arr[5], arr[4]];
+
+    console.log(arr.join(' '));
   
 });
