3a0c061b16e837334cb6095079ed26d3493b43bd
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-29 04:09:06 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,17 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+   let n = result.input.split(" ");
+    let arr = [];
+    let count = 0;
+    for (let i = 0; i < n.length; i++) {
+
+        arr.push(n[i]);
+        count++;
+
+    }
+
+
+    console.log(count - 1);
   
 });
