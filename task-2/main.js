1ee1214c8a17961d9cbf04c9253daa83b5fd704b
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-31 16:56:07 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,18 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    
+    let numbers = result.input.split(" ");
+    let max = parseInt(numbers[0]);
+    let index = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        let element = parseInt(numbers[i])
+        if (element > max) {
+            max = numbers[i]
+            index = i
+        }
+
+
+    } console.log(`${max}\n${index + 1}`);
   
 });
