0f38d33f9d673b6b3c308d96d3f791c78bc34e1f
Isa <110451592+Isa-666@users.noreply.github.com>
2022-08-31 17:54:09 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,6 +4,16 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+        let numbers = result.input.split(" "); //14 16 3 7 17 19 9
+    let arr = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        const element = parseInt(numbers[i]);
+        const element2 = parseInt(numbers[i + 1]);
+        if (element2 > element) {
+            arr.push(element2)
+        }
+    }
+    console.log(arr.join(' '));
   
 });
